4fd3aa94c85aea1f0b11af9e226f7d1cea506fbf
Randy Steele <steelerx155@gmail.com>
2019-10-09 20:11:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(speak)
  if speak == speak.upcase
    return "I LOVE YOU TOO PUMPKIN!"
- elsif speak !! speak.downcase
+ elsif speak != speak.downcase
   return "HUH?! SPEAK UP, SONNY!"
  else
    return "NO, NOT SINCE 1938!"
