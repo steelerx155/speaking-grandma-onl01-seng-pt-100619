e6f2f04bde8e1ac4c518abedd460171f4567ef49
Randy Steele <steelerx155@gmail.com>
2019-10-09 17:43:00 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,10 @@
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"  
+end  
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
